5affa5fd31d3d9ba776d2019d1d5f2bc1b7eb9df
DCrawley94 <danikacrawley.dev@gmail.com>
2024-08-13 13:45:46 +0100

    remove decorators from helpers tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,18 +6,17 @@ from src.helpers_tasks import get_quote, get_parameter
 class TestGetQuote:
     """Tests the get_quote helper."""
 
-    @pytest.mark.it("unit test: get_quote returns correctly formatted dict")
-    def test_get_quote_dict(self, sample_quote_list, result_quote_1):
+    def test_get_quote_returns_status_and_formatted_dict(
+        self, sample_quote_list, result_quote_1
+    ):
         with patch("src.helpers_tasks.random.choice") as mock_choice:
             mock_choice.return_value = sample_quote_list[0]
             assert get_quote() == (200, result_quote_1)
 
-    @pytest.mark.it("integration test: get_quote gets valid quote")
     def test_get_quote_valid_url(self):
         status_code, _ = get_quote()
         assert status_code == 200
 
-    @pytest.mark.it("unit test: returns correct message if non 200 response")
     def test_get_quote_error_response(self):
         with patch(
             "src.helpers_tasks.random.choice",
